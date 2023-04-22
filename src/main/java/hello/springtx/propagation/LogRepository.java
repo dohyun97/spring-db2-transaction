e4e68a1fb305ba85bb4ff64c8711f2b0acce937a
dohyun97 <dohyun647@gmail.com>
2023-04-22 15:35:08 -0400

    Use seperate transaction

---
+++ src/main/java/hello/springtx/propagation/LogRepository.java
@@ -0,0 +1,32 @@
+package hello.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class LogRepository {
+    private final EntityManager em;
+
+    @Transactional
+    public void save(Log logMessage){
+        em.persist(logMessage);
+        if(logMessage.getMessage().contains("LogException")){
+            log.info("Exception in save log");
+            throw new RuntimeException("Runtime Exception");
+        }
+    }
+
+    public Optional<Log> find(String message){
+        String jpql = "select i from Log i where i.message=:message";
+        return em.createQuery(jpql,Log.class)
+                 .setParameter("message",message)
+                .getResultList().stream().findAny();
+    }
+}
---
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,54 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * MemberService    @Transactional:OFF
+     * MemberRepository @Transactional:ON
+     * LogRepository    @Transactional:ON
+     * 각각 트렌젝션을 갖고 커밋
+     */
+    @Test
+    void outerTxOff_success(){
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService    @Transactional:OFF
+     * MemberRepository @Transactional:ON
+     * LogRepository    @Transactional:ON
+     * 각각 트렌젝션을 갖고 LogRepository 롤백
+     */
+    @Test
+    void outerTxOff_fail(){
+        String username = "LogException_outerTxOff_fail";
+
+       assertThatThrownBy(()->memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        //완전히 롤백되지 않고, member 데이터가 남아서 저장된다. try catch로 예외 잡았으니깐
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+}
